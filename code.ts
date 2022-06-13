0f5ea49b00a46287ec6f4d713cb957dff9365d6a
Sergey Zubyk <zubyk.sergey@gmail.com>
2022-06-13 14:40:58 +0300

    added relaunch buttons and ability to delete them

--- code.ts
+++ code.ts
@@ -10,13 +10,49 @@ const generateEmbedLink = () => {
   return null;
 };
 
-const run = async () => {
-  const link = generateEmbedLink();
-  console.log(link, "generateEmbedLink");
-
-  figma.showUI(__html__, {
-    width: 800,
-    height: 600,
-  });
+const addLinkToElement = (link) => {
+  const { selection } = figma.currentPage;
+  for (const node of selection) {
+    console.log(node)
+    node.setRelaunchData({ viewLink: '' })
+    node.setPluginData(node.id, link)
+  }
 };
-run();
+
+figma.ui.onmessage = msg => {
+  if (msg.type === 'add-link') {
+    addLinkToElement(msg.link)
+    const link = generateEmbedLink();
+    console.log(link, "generateEmbedLink");
+  }
+  figma.closePlugin();
+};
+
+figma.on('run', ({command}) => {
+  const { selection } = figma.currentPage;
+  console.log(selection);
+  if(command === 'addLink') {
+    figma.showUI(__uiFiles__.ui_addlink, {
+      width: 300,
+      height: 200,
+    });
+  }
+
+  if(command === 'viewLink') {
+    const link = selection[0].getPluginData(selection[0].id)
+    console.log(link)
+    figma.showUI(__uiFiles__.ui_viewlink, {
+      width: 800,
+      height: 600,
+    });
+    figma.ui.postMessage(link)
+  }
+
+  if(command === 'deleteLink') {
+    const { selection } = figma.currentPage;
+    for (const node of selection) {
+      node.setRelaunchData({})
+    }
+    figma.closePlugin();
+  }
+});
